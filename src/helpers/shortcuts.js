b86ef4d6adf8b34cd3d1db5139820a5d21ce62f8
Edy Segura <edysegura@gmail.com>
2022-05-31 21:53:02 -0300

    chore: add comment for zen mode

--- src/helpers/shortcuts.js
+++ src/helpers/shortcuts.js
@@ -1,6 +1,7 @@
 export default function shortcuts() {
   document.addEventListener('keypress', (event) => {
     // console.log('key pressed', event.shiftKey, event.ctrlKey, event.code)
+    // Zen Mode
     if (event.shiftKey && event.ctrlKey && event.code === 'KeyZ') {
       document.querySelector('textarea').classList.toggle('zenmode')
     }
